60e9bacdf76c76c0a4a09321d5822e88c6d0a8ee
projectinfob <43008234+projectinfob@users.noreply.github.com>
2018-09-05 19:08:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,29 @@ import threading
 client1=os.environ['database']
 client=MongoClient(client1)
 db=client.infobase
+users=db.users
 
+   
     
-
-
-
-
+    
+@bot.message_handler(commands=['start'])
+def start(m):
+    if m.from_user.id==m.chat.id:
+        bot.send_message(m.chat.id, 'Здраствуйте, Вас приветствует бот-помощник, который научит Вас самостоятельно писать '+
+                         'ботов для мессенджера Telegram. Если Вас интересует, как установить среду для разработки, нажмите /basetools.\n'+
+                         'Если вы не знаете, как написать сам код для бота, нажмите /codeguide;\nЕсли '+
+                         'Вас интересует то, где бесплатно организовать хостинг для бота (его постоянную работу), нажмите /hosting.\n'+
+                         'Ответы на частозадаваемые вопросы вы можете найти по команде /answers.')
+  
+    
+@bot.message_handler(commands=['basetools'])
+def basetools(m):
+    if m.from_user.id==m.chat.id:
+        bot.send_message(m.chat.id, 'Чтобы скачать python с оффициального сайта, нажмите на ссылку ниже.\n'+
+                         'https://www.python.org/downloads/release/python-361/'+'\nЗатем пролистайте сайт вниз, до надписи FILES.'+
+                         'Там скачайте `Windows x86 executable installer` и установите .exe файл.')
+    
+    
     
     
 
