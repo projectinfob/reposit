e0bc704db92bdbcc2849dd4d1eade4d619984ad2
projectinfob <43008234+projectinfob@users.noreply.github.com>
2018-10-25 14:07:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,30 +32,56 @@ def start(m):
 
 @bot.message_handler()
 def channelselect(m):
+    x=channels.find_one({})
+    user=users.find_one({'id':m.from_user.id})
+    if m.text=='Далее':
+        users.update_one({'id':user['id']},{'$inc':{'currentindex':3}})
+        user=users.find_one({'id':m.from_user.id})
+        y=x[user['currenttheme']]
+        text=showchannels(user,y)
+        kb=types.ReplyKeyboardMarkup()
+        kb.add(types.ReplyKeyboardButton('Назад'),types.ReplyKeyboardButton('Далее'))
+        bot.send_message(m.chat.id, text, reply_markup=kb)
+        
+        
     if m.text=='МУЗЫКА':
-        x=channels.find_one({})
         y=x['music']
         channel=0
         text=''
-        i=channel+3
-        while channel<i:
-            ch=y[channel]
-            text+='Рекламодатель: '+ch['reklamodatel']+'\n'
-            text+='Канал: '+ch['channel']+'\n'
-            text+='Подписчиков: '+str(ch['subs'])+'\n'
-            text+='Цена: '+str(ch['cost'])+'\n'
-            text+='Скидка: '+str(ch['discount'])+'\n'
-            text+='Итоговая цена: '+str(ch['finalcost'])+'\n'
-            text+='Тематика: '+themetoname(ch['theme'])+'\n'
-            text+='Взаимный пиар: '+yesno(ch['piar'])+'\n'
-            text+='Условия: '+ch['conditions']+'\n'
-            text+='Для заказа рекламы тебе стоит написать администратору канала.\n'
-            text+='\n'
-            channel+=1
+        users.update_one({'id':m.from_user.id},{'$set':{'currenttheme':'music'}})
+        users.update_one({'id':m.from_user.id},{'$set':{'currentindex':0}})
+        user=users.find_one({'id':m.from_user.id})
+        
+        text+=showchannels(user,y)
+        
         kb=types.ReplyKeyboardMarkup()
-        kb.add(types.ReplyKeyboardButton(''),types.ReplyKeyboardButton(''))
+        kb.add(types.ReplyKeyboardButton('Назад'),types.ReplyKeyboardButton('Далее'))
+        bot.send_message(m.chat.id, text, reply_markup=kb)
             
-            
+    
+def showchannels(user, y):
+    channel=user['currentindex']
+    i=channel+3
+    while channel<i:
+      try:
+        ch=y[channel]
+        text+='Рекламодатель: '+ch['reklamodatel']+'\n'
+        text+='Канал: '+ch['channel']+'\n'
+        text+='Подписчиков: '+str(ch['subs'])+'\n'
+        text+='Цена: '+str(ch['cost'])+'\n'
+        text+='Скидка: '+str(ch['discount'])+'\n'
+        text+='Итоговая цена: '+str(ch['finalcost'])+'\n'
+        text+='Тематика: '+themetoname(ch['theme'])+'\n'
+        text+='Взаимный пиар: '+yesno(ch['piar'])+'\n'
+        text+='Условия: '+ch['conditions']+'\n'
+        text+='Для заказа рекламы тебе стоит написать администратору канала.\n'
+        text+='\n'
+        channel+=1
+      except:
+            pass
+    return text
+    
+    
 
 if True:
    print('bot is working')
